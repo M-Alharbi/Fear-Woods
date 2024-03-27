4b5455382b55667ceaacce5fa3cf5c2a9353b2e8
M-Alharbi <mr.alharbi2031@gmail.com>
2024-03-27 07:36:53 -0400

    adding map, car, etc

---
+++ Code/fear-woods/Assets/Cartoon SportCar B01/script/OpenElement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenElement : MonoBehaviour {
+
+	 private Animator Opening;
+    public KeyCode keyboard;
+    
+    void Start () {
+        Opening = GetComponent<Animator>();
+        // Set the initial state to open
+        Opening.SetInteger("EtatAnim", 1);
+    }
+    
+    void Update () {
+        // You may remove this condition if you want the element to always stay open
+        if (Input.GetKeyDown(keyboard)) {
+            // This block ensures that the element stays open
+            if (Opening.GetInteger("EtatAnim") != 1) {
+                Opening.SetInteger("EtatAnim", 1);
+            }
+        }
+    }
+}
---
+++ Code/fear-woods/Assets/Cartoon SportCar B01/script/on_off_light.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class on_off_light : MonoBehaviour
+{
+
+	 public Light[] lights;
+
+    void Start()
+    {
+        // Turn on all lights when the script starts
+        foreach (Light light in lights)
+        {
+            light.enabled = true;
+        }
+    }
+}
+
